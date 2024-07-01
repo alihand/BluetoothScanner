d46749fb0870d3d449646af02557b4b5ea4d9197
alihanndemirr <ali.demir@beymen.com>
2024-07-01 19:48:19 +0300

    Initial Commit

---
+++ BluetoothScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothScanner
+//
+//  Created by Ali Han DEMIR on 1.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
